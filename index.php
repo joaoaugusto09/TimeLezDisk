a675fd6b2d952ba996a71118db4fd1797ed83f04
Thiago Mello <>
2022-08-18 17:21:25 -0300

    Att 18/08
    muitas coisas

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>TimeLezDisks</title>
+</head>
+<body>
+    
+    <header>
+        <h1>TimeLezDisks</h1>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="produtos.php">Produtos</a></li>
+            <li><a href="contato.php">Contato</a></li>
+            <li><a href="rock.php" target="_blank">ROCK</a></li>
+        </ul>
+    </header>
+    <main>
+        <div id="banner">
+            <div id="legenda">
+            <h2>A maior loja online de Discos de Vinil da Cavalhada Zona sul de Porto Alegre</h2>
+            </div>
+        </div>
+        <div class="card"></div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ rock.php
@@ -0,0 +1,5 @@
+       
+        <iframe width="100%" height="100%"
+        src="https://www.youtube.com/embed/E0ozmU9cJDg?autoplay=1&mute=0">
+        </iframe>
+        </body>
