e69dd45d78bebd078d13f60a0f74fbf259ee9602
João Augusto Carbonera <joaocarbonera06@gmail.com>
2022-08-21 16:31:20 -0300

    Att 21/08

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     
     <header>
-        <h1>TimeLezDisks</h1>
+        <img src="images/logo (2).png" alt="">
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="produtos.php">Produtos</a></li>
@@ -24,7 +24,38 @@
             <h2>A maior loja online de Discos de Vinil da Cavalhada Zona sul de Porto Alegre</h2>
             </div>
         </div>
-        <div class="card"></div>
+        <section>
+        <h1>Adicionados Recentemente</h1>
+        <div class="grid-container">
+            <div class="card">
+                <a href="produtodetalhe.php">
+                <div class="card-img"><img src="images/ledzeppelin.jpg"></div>
+                </a>
+                <div class="card-body">
+                    <h3>Led Zeppelin IV</h3>
+                    <span>R$100 </span>
+                </div>
+            </div>
+            <div class="card">
+                <a href="produtodetalhe.php">
+                <div class="card-img"><img src="images/pinkfloyd.jpg"></div>
+                </a>
+                <div class="card-body">
+                    <h3>Pink Floyd - The Dark Side of The Moon</h3>
+                    <span>R$100</span>
+                </div>
+            </div>
+            <div class="card">
+                <a href="produtodetalhe.php">
+                <div class="card-img"><img src="images/metallica.jpg"></div>
+                </a>
+                <div class="card-body">
+                    <h3>Metallica - Master Of Puppets</h3>
+                    <span>R$100</span>
+                </div>
+            </div>
+        </div>
+        </section>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
